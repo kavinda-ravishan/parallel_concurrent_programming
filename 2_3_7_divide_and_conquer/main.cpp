2f377b154bb5601ee3d72a0dade1ed47bf9876b0
kavinda-ravishan <kvnravishan@outlook.com>
2022-08-13 18:12:46 +0530

    Divide and conquer

---
+++ 2_3_7_divide_and_conquer/main.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <future>
+#include <cmath>
+
+unsigned long long recursive_sum(unsigned long long low, unsigned long long high, unsigned int max_depth, unsigned long long depth = 0)
+{
+    if (depth >= max_depth)
+    {
+        unsigned long long sum = 0;
+        printf("CAL -> Low : %lli - high : %lli\n", low, high);
+        for (unsigned long long i = low; i <= high; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+    else
+    {
+        auto mid = (low + high) / 2;
+        auto sum_1 = std::async(std::launch::async, recursive_sum, low, mid - 1, max_depth, depth + 1);
+        auto sum_2 = recursive_sum(mid, high, max_depth, depth + 1);
+        return sum_1.get() + sum_2;
+    }Divide and conquer
+}
+
+int main()
+{
+    unsigned int no_threads = (unsigned int)log2(16);
+    printf("Sum : %lli\n", recursive_sum(1, 100, no_threads));
+
+    return 0;
+}
